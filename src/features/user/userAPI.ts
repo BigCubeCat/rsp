6d94b84911be9c8bb49f8f04c722e55b2d27e3a4
BigCubeCat <bigcubecat@yandex.ru>
2023-02-02 11:07:37 +0700

    refactor redux

--- src/features/user/userAPI.ts
+++ src/features/user/userAPI.ts
@@ -1,4 +1,3 @@
-// A mock function to mimic making an async request for data
 export function fetchUser(name = "guest") {
   return new Promise<{ data: string }>((resolve) =>
     setTimeout(() => resolve({ data: name }), 500)
