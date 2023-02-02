b3c4dd19cb57b27d02b5c73015ffdefde90a7b70
BigCubeCat <bigcubecat@yandex.ru>
2023-02-02 18:48:02 +0700

    mv elems images to elems folder

--- src/components/Game/game_elements.ts
+++ src/components/Game/game_elements.ts
@@ -6,31 +6,31 @@ export interface IElement {
 
 export const ELEMENTS: IElement[] = [
   {
-    name: "rock", img: "/img/rock.png", color: "#fbd165"
+    name: "rock", img: "/elems/rock.png", color: "#fbd165"
   },
   {
-    name: "scissors", img: "/img/scissors.png", color: "#a9cc8a"
+    name: "scissors", img: "/elems/scissors.png", color: "#a9cc8a"
   },
   {
-    name: "paper", img: "/img/paper.png", color: "#f4b1c0"
+    name: "paper", img: "/elems/paper.png", color: "#f4b1c0"
   },
   {
-    name: "lizard", img: "/img/lizzard.png", color: "#a9fba9"
+    name: "lizard", img: "/elems/lizzard.png", color: "#a9fba9"
   },
   {
-    name: "spock", img: "/img/spock.png", color: "#c0b1f4"
+    name: "spock", img: "/elems/spock.png", color: "#c0b1f4"
   },
   {
-    name: "unicorn", img: "/img/cat.png", color: "#c4c4f4"
+    name: "unicorn", img: "/elems/cat.png", color: "#c4c4f4"
   },
   {
-    name: "fire", img: "/img/fire.png", color: "#fbc0c0"
+    name: "fire", img: "/elems/fire.png", color: "#fbc0c0"
   },
   {
-    name: "donut", img: "/img/donut.png", color: "#b1c0f4"
+    name: "donut", img: "/elems/donut.png", color: "#b1c0f4"
   },
   {
-    name: "surfer", img: "/img/cool.png", color: "#f4f4c0",
+    name: "surfer", img: "/elems/cool.png", color: "#f4f4c0",
   }
 ]
 
