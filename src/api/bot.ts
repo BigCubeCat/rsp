4b765f2ca4ad128c92d4c311e7addbc20057d65e
Bitochkin Egor <56190993+BigCubeCat@users.noreply.github.com>
2023-08-08 21:03:05 +0700

    Update bot.ts

--- src/api/bot.ts
+++ src/api/bot.ts
@@ -1,33 +1,17 @@
-const API_URL = "https://shift-winter-2023-backend.onrender.com/api/rsp/option";
-
-async function getBotAnswer(options: string[]): Promise<string> {
-  const result = await fetch(API_URL, {
-    method: "POST", headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify({ "options": options })
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-      return response.json()
-    })
-    .then(json => json.result)
-  return result;
+function getRandomElement(items: string[]): string {
+  return items[Math.floor(Math.random()*items.length)];
 }
 
 async function GetClassic(): Promise<string> {
-  return getBotAnswer(["rock", "scissors", "paper"]);
+  return getRandomElement(["rock", "scissors", "paper"]);
 }
 
 async function GetSpock(): Promise<string> {
-  return getBotAnswer(["rock", "scissors", "paper", "lizard", "spock"]);
+  return getRandomElement(["rock", "scissors", "paper", "lizard", "spock"]);
 }
 
 async function GetCustom(options: string[]): Promise<string> {
-  return getBotAnswer(options);
+  return getRandomElement(options);
 }
 
 
